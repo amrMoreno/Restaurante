475eccbd5342ad08b0d99ef85920475a2fae1344
amrMoreno <adrianmorenoruiz@gmail.com>
2019-04-26 14:25:34 +0200

    26/04/2019

--- Restaurante/src/clases/Camarero.java
+++ Restaurante/src/clases/Camarero.java
@@ -5,7 +5,6 @@
  */
 package clases;
 
-
 import excepciones.DniInvalidoException;
 import java.time.LocalDate;
 
@@ -14,25 +13,23 @@ import java.time.LocalDate;
  * @author 1DAM
  */
 public class Camarero {
+
     private String dni;
     private String nombre;
-   private String tipoDeCamarero;
-   
-   
+    private String tipoDeCamarero;
 
-    public Camarero(String dni, String nombre , String tipoDeCamarero) throws DniInvalidoException {
+    public Camarero(String dni, String nombre, String tipoDeCamarero) throws DniInvalidoException {
         setDni(dni);
         this.nombre = nombre;
         this.tipoDeCamarero = tipoDeCamarero;
     }
-
 
     public String getDni() {
         return dni;
     }
 
     public void setDni(String dni) throws DniInvalidoException {
-       if (dni.length() != 9) {
+        if (dni.length() != 9) {
             throw new DniInvalidoException("El DNI ha fallado");
         }
         this.dni = dni;
@@ -47,8 +44,6 @@ public class Camarero {
         this.nombre = nombre;
     }
 
-   
-
     public String getTipoDeCamarero() {
         return tipoDeCamarero;
     }
@@ -56,5 +51,5 @@ public class Camarero {
     public void setTipoDeCamarero(String TipoDeCamarero) {
         this.tipoDeCamarero = TipoDeCamarero;
     }
-    
+
 }
---
+++ Restaurante/src/vista/Camareros.java
@@ -0,0 +1,53 @@
+package vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Camareros extends JFrame {
+
+	private JPanel contentPane;
+
+
+
+	/**
+	 * Create the frame.
+	 */
+	public Camareros() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 200);
+		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.controlHighlight);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Nuevo Camarero");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(305, 94, 119, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Eliminar Camarero");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(305, 128, 119, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.controlHighlight);
+		panel.setBounds(0, 11, 295, 140);
+		contentPane.add(panel);
+	}
+}
